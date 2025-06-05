f9ffef87790684696fce16d01d7db7dc91dbc44e
Nahjs <lenovo-k91@outlook.com>
2025-06-06 06:55:54 +0800

    更新后端配置，切换至新的生产服务器地址，确保API和WebSocket连接安全性提升。

--- backend.config.js
+++ backend.config.js
@@ -1,8 +1,14 @@
 const BackendConfig = {
   // --- Deployed Server Configuration ---
-  BASIC_URL: 'http://1.92.122.228',
-  RESTFUL_API_URL: 'http://1.92.122.228/api',
-  WebSocket_URL: 'ws://1.92.122.228/ws/chat',
+  BASIC_URL: 'https://api.siyuantao.xyz',
+  RESTFUL_API_URL: 'https://api.siyuantao.xyz/api',
+  WebSocket_URL: 'wss://api.siyuantao.xyz/ws/chat',
+
+
+  // --- Deployed Server Configuration ---
+  // BASIC_URL: 'http://1.92.122.228',
+  // RESTFUL_API_URL: 'http://1.92.122.228/api',
+  // WebSocket_URL: 'ws://1.92.122.228/ws/chat',
 
   // --- Local Development Configuration ( uncomment to use ) ---
   // BASIC_URL: 'http://127.0.0.1:8001', // 本地默认端口
